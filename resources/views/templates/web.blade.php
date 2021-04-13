cead9606702cc06f48d29ae42094bad5a5763c97
Ajay <ajaymaurya.it@gmail.com>
2021-04-13 09:40:46 +0530

    footer social

--- resources/views/templates/web.blade.php
+++ resources/views/templates/web.blade.php
@@ -121,14 +121,29 @@
         <div class="col-md-3">
           <h4>Follow Us</h4>
           <ul class="list-inline mt-4">
-            <li class="list-inline-item"> <a href="#"><i class="fa wp-icon fa-facebook-f"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fa wp-icon fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fa wp-icon fa-linkedin"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fa wp-icon fa-pinterest "></i></a></li>
+            @if(setting('site.mobile'))
+            <li class="list-inline-item mr-4"><a  href="tel:{{str_replace(' ','',str_replace('-','',setting('site.mobile')))}}" title="" data-toggle="tooltip" data-original-title="Call us {{setting('site.mobile')}} OR Contact with us @ support@registrationwala.com"><i class="fa fa-mobile mobile"></i> {{setting('site.mobile')}}</a></li>
+            @endif
+            @if(setting('site.facebook_link'))
+            <li class="list-inline-item"><a href="{{setting('site.facebook_link')}}" target="_blank" class="d-none d-lg-block"><i class="fa wp-icon fa-facebook-f"></i></a></li>
+            @endif
+            @if(setting('site.youtube_link'))
+            <li class="list-inline-item"><a href="{{setting('site.youtube_link')}}" target="_blank"  class="d-none d-lg-block"><i class="fa wp-icon fa-youtube "></i></a></li>
+            @endif
+            @if(setting('site.twitter_link'))
+            <li  class="list-inline-item"><a href="{{setting('site.twitter_link')}}" target="_blank"  class="d-none d-lg-block"><i class="fa wp-icon fa-twitter"></i></a></li>
+            @endif
+            @if(setting('site.linkedin_link'))
+            <li class="list-inline-item"><a href="{{setting('site.linkedin_link')}}" target="_blank"  class="d-none d-lg-block"><i class="fa wp-icon fa-linkedin"></i></a></li>
+            @endif
           </ul>
-          <ul class="list-unstyled mt-4">
-            <li class="mb-2"><i class="fa  fa-whatsapp" aria-hidden="true"></i> <a href="tel:9810602899"> +91-9810-602-899</a></li>
-             <li class="mb-2"><i class="fa fa-phone-square" aria-hidden="true"></i><a href="tel:8882580580"> +91-8882-580-580</a></li>
+          <ul class="list-unstyled mt-4">            
+            @if(setting('site.whatsaap'))
+            <li class="mb-2"><i class="fa  fa-whatsapp" aria-hidden="true"></i> <a href="https://api.whatsapp.com/send?phone={{str_replace(' ','',str_replace('-','',setting('site.whatsaap')))}}&amp;text=Lets%20talk%20to%20Registrationwala!"> {{setting('site.whatsaap')}}</a></li>
+            @endif
+            @if(setting('site.mobile'))
+             <li class="mb-2"><i class="fa fa-phone-square" aria-hidden="true"></i><a href="tel:{{str_replace(' ','',str_replace('-','',setting('site.mobile')))}}"> {{setting('site.mobile')}}</a></li>
+             @endif
 
             <li> <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:support@registrationwala.com"> support@registrationwala.com</a></li>
           </ul>
