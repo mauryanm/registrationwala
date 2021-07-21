455dd17e19a4b28f774a25123577e5df1a25ba98
Ajay <ajaymaurya.it@gmail.com>
2021-07-21 22:25:57 +0530

    service mail enq 2.3

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -84,6 +84,16 @@ class MailController extends Controller {
         );
           $this->send_mail($mail_enq);
         }
+        if($request->input('source')=='subscribe'){
+          $mail_sub=array(
+            'to'=>$request->email,
+            'from_name'=>setting('admin.title'),
+            'from'=>setting('site.site_mail'),
+            'subject'=>'We are happy to help you ! Registrationwala.com',
+            'message'=>$this->subscribe($insData->email)
+          );
+          $this->send_mail($mail_sub);
+        }
       $response = array('type' => 'success',"title"=>"",'msg'=>['Your query has been submitted successfully.']);
         return response()->json($response);
 
@@ -265,7 +275,75 @@ class MailController extends Controller {
 </html>
 ';
 return $srhtml;
-   }
+}
+
+  private function subscribe($name){
+    return '<table width="100%" cellpadding="0" cellspacing="0">
+<tr><td>
+<table style="margin:auto; width:600px; font-size:16px; line-height:24px; font-family:Verdana, Geneva, sans-serif" cellpadding="0" cellspacing="0">
+<tr><td>
+<table width="100%" cellpadding="5" cellspacing="0">
+<tr><td><a href="https://www.registrationwala.com"><img src="https://www.registrationwala.com/images/emailer/logonrw.png" width="109" height="38" alt="Registrationwala.com" /></a></td>
+<td align="right"><a style="margin:0 5px;" href="https://www.facebook.com/registrationwala/" target="_blank"><img src="https://www.registrationwala.com/images/emailer/facebookc.png" width="27" height="27" alt="Like On Facebook" /></a><a style="margin:0 5px;" href="https://twitter.com/Registrationwla" target="_blank"><img src="https://www.registrationwala.com/images/emailer/twitterc.png" width="27" height="27" alt="Like On Twitter" /></a><a style="margin:0 5px;" href="https://plus.google.com/u/0/115063389280026230269/posts" target="_blank"><img src="https://www.registrationwala.com/images/emailer/g-plusc.png" width="27" height="27" alt="Like On Google plus" /></a></td></tr>
+</table>
+</td></tr>
+<tr><td>
+<table width="100%" cellpadding="15" cellspacing="0">
+<tr><td style="border:1px solid #ccc; background:url(https://www.registrationwala.com/images/emailer/drop-mark.png) no-repeat top left 10px;">
+<table width="100%" cellpadding="5" cellspacing="0">
+<tr><td style="padding:10px 20px;">
+<h1 style="margin:8px; padding-left:25px; font-size:40px; font-weight:300; text-transform:uppercase; color:#00293c; font-family:\'Times New Roman\', Times, serif">Welcome</h1>
+<p style="padding-left:25px; margin:8px; text-transform:uppercase; font-size:13px;">Thank You for signing up with us</p>
+</td></tr>
+<tr><td style="padding:10px 20px;">
+<img src="https://www.registrationwala.com/images/emailer/welcome-bg.jpg" height="215" width="483" style="height:auto; width:100%" alt="Welcome" />
+</td></tr>
+<tr><td style="padding:10px 20px; background-color:#00293c; color:#fff;" bgcolor="#00293c">
+<p style="padding-left:10px; margin:0">Dear '.$name.',</p>
+</td></tr>
+<tr><td>
+
+<table width="100%" cellpadding="0" cellspacing="0">
+
+<tr><td style="padding:10px 20px;">
+<p style="margin:0; color:#00293c;"><strong>Congratulations on taking the first step towards success!</strong></p>
+</td></tr>
+<tr><td style="padding:10px 20px;">
+<p style="margin:0; text-align:justify;">We wholeheartedly welcome you to our family of Registrationwala. Thank you for subscribing to our newsletter. We have received your request for newsletter subscription and we are glad to inform you that we have successfully added your e-mail id to our subscriber\'s list. This newsletter is intended to keep the readers updated about the recent happening in the corporate world.</p>
+
+<p style="margin:0; text-align:justify;">Registrationwala is a leading business consultant platform in India offering the comprehensive solutions for all business requirements.  Our expert team comprises of the charted accountants, company secretary, lawyers and business experts assisting over 15000 clientsyearly to successfully start and run their business. We strive hard to become the ‘partner of choice’ when it comes to incorporating and managing business in India.</p>
+<p style="margin:0; text-align:justify;"><strong>What are services offered by Registrationwala?</strong></p>
+<p style="margin:0; text-align:justify;">Registrationwala specializes in providing the comprehensive services related to company incorporation, IPR, Accounting and other certifications. Below we have listed out some of the services offered by Registrationwala-</p>
+<p style="margin:0; text-align:justify;"><ul>
+  <li><a href="https://www.registrationwala.com/company-registration">Company  Registration</a></li>
+  <li><a href="https://www.registrationwala.com/trademark-registration">Trademark  Registration</a></li>
+  <li><a href="https://www.registrationwala.com/iso-registration">ISO  Certification</a></li>
+  <li><a href="https://www.registrationwala.com/dot-osp-registration">DOT OSP  License for BPO</a><a name="_GoBack"></a></li>
+  <li><a href="https://www.registrationwala.com/accounting-and-book-keeping">Accounting &amp;  Bookkeeping Services</a> and <a href="https://www.registrationwala.com">More…</a></li>
+</ul></p>
+
+</td></tr>
+</table>
+
+</td></tr>
+<tr>
+<td style="padding:15px 10px; background-color:#00293c;">
+<h2 style="color:#fff; margin:0 0 20px 0; padding:0; font-size:18px; text-align:center; text-transform:uppercase;">Further, for any queries feel free to write us </h2> 
+<table width="100%" cellpadding="0" cellspacing="0">
+<tr>
+<td align="center"><a style="text-decoration:none; color:#fff;" href="mailto:support@registrationwala.com">support@registrationwala.com</a></td><td align="center"><a style="text-decoration:none; color:#fff;" href="tel:+918882580580">+91-888-2580-580</a></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td></tr>
+</table>
+</td></tr>
+</table>
+</td></tr>
+</table>';
+  }
 
    
 }
