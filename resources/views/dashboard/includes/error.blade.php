3df14d647c509dafdc703536d36efc9b79ab39d4
Ajay <ajaymaurya.it@gmail.com>
2021-07-21 20:54:53 +0530

    error reporting

--- resources/views/dashboard/includes/error.blade.php
+++ resources/views/dashboard/includes/error.blade.php
@@ -1,5 +1,5 @@
 @if($errors->any())
-{{ implode('', $errors->all('<div>:message</div>')) }}
+{!! implode('', $errors->all('<div class="alert alert-danger">:message</div>')) !!}
 @endif
 @foreach (['danger', 'warning', 'success', 'info'] as $key)
 @if(Session::has($key))
