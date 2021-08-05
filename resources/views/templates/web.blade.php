1019e052b8f1bf3519dc1ccaafd9ea15c22b839a
Ajay <ajaymaurya.it@gmail.com>
2021-08-05 22:35:37 +0530

    meta

--- resources/views/templates/web.blade.php
+++ resources/views/templates/web.blade.php
@@ -4,7 +4,7 @@
   <base target="_parent">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="@yield('description')">
+  <meta name="description" content="@yield('description',setting('site.description'))">
   <meta name="keywords" content="@yield('keywords')">
   <meta name="author" content="@yield('author')">
   <meta name="csrf-token" content="{{ csrf_token() }}">
