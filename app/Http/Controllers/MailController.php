00a200bf291dca2ce8051b6f50e1732ad1f54279
Ajay <ajaymaurya.it@gmail.com>
2021-08-06 22:03:52 +0530

    testing support mail 2

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -35,7 +35,7 @@ class MailController extends Controller {
         //     'message'=>$this->rwsupportmail($request->except('_token','_method','page_id'))
         // );
         // \Log::info($mailto);
-       $this->sendMail($mailto);
+       // $this->sendMail($mailto);
         $mail_arry=array(
     			'to'=>$request->email,
     			'from_name'=>setting('admin.title'),
