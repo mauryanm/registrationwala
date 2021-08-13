c3fb602e069b15d56c9c7cabe3497f9195981e8c
Ajay <ajaymaurya.it@gmail.com>
2021-08-13 11:18:45 +0530

    dashboard

--- resources/views/templates/dashboard.blade.php
+++ resources/views/templates/dashboard.blade.php
@@ -11,7 +11,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="robots" content="noindex">
   @yield('og')
-  <link rel="icon" href="{{ asset('/images/icon/favicon.png') }}" type="image/gif" >  
+  <link rel="icon" href="{{ asset('/assets/images/icon/favicon.png') }}" type="image/gif" >  
   <link rel="manifest" href="/manifest.json">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
@@ -20,23 +20,23 @@
   <meta name="theme-color" content="#e5e5e5">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-  <script src="{{ asset('/js/jquery.min.js') }}"></script>
-  <script src="{{ asset('/js/popper.min.js') }}"></script>
-  <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
-  <link rel="stylesheet" href="{{ asset('/css/font-awesome.min.css') }}" crossorigin="anonymous" />
-  <link rel='stylesheet' href="{{ asset('/css/owl.carousel.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('/css/user-dashboard.css') }}">
+  <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+  <script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('/assets/js/popper.min.js') }}"></script>
+  <script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
+  <link rel="stylesheet" href="{{ asset('/assets/css/font-awesome.min.css') }}" crossorigin="anonymous" />
+  <link rel='stylesheet' href="{{ asset('/assets/css/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/assets/css/user-dashboard.css') }}">
 {!! setting('site.header_script') !!}
  </head>
  <body>
   @yield('content')
 
-<script src="{{ asset('/js/owl.carousel.min.js') }}"></script> 
-<script src="{{ asset('/js/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('/js/sweetalert.js') }}"></script>
-<script src="{{ asset('/js/additional-methods.min.js') }}"></script>
-{{-- <script src="{{ asset('/js/custom.js') }}"></script> --}}
+<script src="{{ asset('/assets/js/owl.carousel.min.js') }}"></script> 
+<script src="{{ asset('/assets/js/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('/assets/js/sweetalert.js') }}"></script>
+<script src="{{ asset('/assets/js/additional-methods.min.js') }}"></script>
+{{-- <script src="{{ asset('/assets/js/custom.js') }}"></script> --}}
 @yield('script')
 {!! setting('site.footer_script') !!}
  </body>
